7d6ffffe378b6f99e3c2411d3445d2a6b5129b18
Fausto Gregorio <fausto.g03@gmail.com>
2020-03-05 00:01:15 -0600

    parser v1.0

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {parse} from '../assets/js/truth-table-parser.js';
+//var parser = require('../assets/js/parser.js');
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'truth-tables';
+
+  prueba() {
+    try {
+      let result = parse('p→(q´˄r˅p´)´');
+      console.log('result', result);
+    } catch (error) {
+      console.log('error', error.message);
+    }
+    //console.log("prueba", parse('2+2/2^2').message ? 'mal' : 'bien');
+  }
 }
