011652e0a59115a674ddd3404a42c132b6e7f573
Fausto Corvus <grefausto@gmail.com>
2022-07-28 03:25:45 -0500

    cleaning project

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,13 +3,6 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  apiKey: "AIzaSyDW4dh7HRn-0nAal8OR8JAqyi5InnlZl7o",
-  authDomain: "truth-table-generator-e587b.firebaseapp.com",
-  projectId: "truth-table-generator-e587b",
-  storageBucket: "truth-table-generator-e587b.appspot.com",
-  messagingSenderId: "89958249860",
-  appId: "1:89958249860:web:6fc17e122c5ae84cd559d1",
-  measurementId: "G-ZYT2BHZ7TG",
   production: false
 };
 
